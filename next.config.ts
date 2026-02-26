634a0d2cb258ad3d6b1d254c058fa468c8bac80c
Chris <chris@example.com>
2026-02-26 10:41:11 +0100

    fix: add basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
+  basePath: '/qualtio-tech-radar',
   images: {
     unoptimized: true,
   },
